a6f974b18b0077bdb00e005f51a82ffc02473738
Raj Kumar <raj.kuntal09@gmail.com>
2016-07-01 13:02:33 +0530

    table name changes to admin_store.

--- src/main/java/com/eparchuniya/app/domain/Store.java
+++ src/main/java/com/eparchuniya/app/domain/Store.java
@@ -12,7 +12,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "adm_store")
+@Table(name = "admin_store")
 public class Store implements Serializable{
 	
 	/**
